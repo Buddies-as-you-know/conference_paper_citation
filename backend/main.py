3ec07cd3eb469f52715472c9d7dbef936b078223
YuminosukeSato <sicops@i.softbank.jp>
2023-10-31 19:03:12 +0900

    test pass

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,18 @@
 import json
+import logging
+import pathlib
 from typing import Dict
+
 import requests
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request  # <- Formを追加
 from fastapi.responses import JSONResponse, Response
 from fastapi.templating import Jinja2Templates
-import pathlib
-from logging import getLogger
-logger = getLogger(__name__)
-PATH_TEMPLATES = str(
-    pathlib.Path(__file__).resolve() \
-        .parent.parent / "backend/templates"
-)
+
+logging.basicConfig(level=logging.INFO)
+PATH_TEMPLATES = str(pathlib.Path(__file__).resolve().parent.parent / "backend/templates")
 app = FastAPI()
 templates = Jinja2Templates(directory=PATH_TEMPLATES)
+
 
 @app.get("/")
 async def read_root(request: Request) -> Response:
@@ -20,9 +20,8 @@ async def read_root(request: Request) -> Response:
 
 
 @app.post("/search/")
-async def search_venue(venue: str) -> JSONResponse:
-    
-    endpoint = 'https://api.semanticscholar.org/graph/v1/paper/search/bulk'
+async def search_venue(venue: str = Form(...)) -> JSONResponse:
+    endpoint = "https://api.semanticscholar.org/graph/v1/paper/search/bulk"
     fields = (
         "title",
         "year",
@@ -34,13 +33,28 @@ async def search_venue(venue: str) -> JSONResponse:
         "authors",
         "venue",
     )
-    params : Dict[str, str] = {'venue': venue, 'fields': ",".join(fields)}
-    logger.info(params)
+    params: Dict[str, str] = {"venue": venue, "fields": ",".join(fields)}
+
+    # APIへのリクエスト情報をログに記録
+    logging.info(f"Sending request to {endpoint} with parameters: {params}")
+
     r = requests.get(url=endpoint, params=params)
-    print(r)
+
+    # レスポンスのステータスコードをログに記録
+    logging.info(f"Received response with status code {r.status_code}")
+
+    if r.status_code != 200:
+        logging.error(f"Error in API response: {r.text}")
+        return JSONResponse(
+            content={"error": "API request failed", "details": r.text}, status_code=r.status_code
+        )
+
     r_dict = json.loads(r.text)
     total = r_dict["total"]
     data = r_dict["data"]
     sorted_data = sorted(data, key=lambda x: int(x["influentialCitationCount"]), reverse=True)
+
+    # 結果の要約をログに記録
+    logging.info(f"Total papers found: {total}")
 
     return JSONResponse(content={"total": total, "sorted_data": sorted_data})
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,28 @@
 from fastapi.testclient import TestClient
-from backend.main import app  # プロジェクトのディレクトリ構造に応じて適切なインポートパスを使用してください。
+
+from backend.main import (
+    app,  # プロジェクトのディレクトリ構造に応じて適切なインポートパスを使用してください。
+)
 
 client = TestClient(app)
 
-def test_read_root()->None:
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "request" in response.text
 
-def test_search_venue()->None:
-    response = client.post("/search/", json={"venue": "IEEE International Conference on Robotics and Automation"})
+def test_search_venue() -> None:
+    # 正常なリクエストのテスト
+    response = client.post(
+        "/search/", data={"venue": "IEEE International Conference on Robotics and Automation"}
+    )
     assert response.status_code == 200
-    assert response.json()["total"] is not None
-    assert isinstance(response.json()["sorted_data"], list)
+    data = response.json()
+    assert "total" in data
+    assert "sorted_data" in data
 
+    # APIエラーのテスト（例：存在しないvenueを指定）
+    response = client.post("/search/", data={"venue": "NonExistentVenue"})
+    assert response.status_code == 400  # もしくはAPIが返す適切なエラーコード
+    assert "error" in response.json()
+
+    # venueパラメータがない場合のテスト
+    response = client.post("/search/")
+    assert response.status_code == 422
+    assert "detail" in response.json()
